b16081c838f5e1119e8c126af6cfbe47048ff94e
Iamsteveameh <atagastephen2@gmail.com>
2022-09-28 17:18:54 -0700

    fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -14,6 +14,7 @@ def search_replace(my_list, search, replace):
         replace if x == search else x for x in my_list
     ]
 
+
 if __name__ == '__main__':
     my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
     new_list = search_replace(my_list, 2, 89)
