ae33aaec5dd256d40859914a23dbd09fe66e6d5e
Iamsteveameh <atagastephen2@gmail.com>
2022-10-12 18:44:42 -0700

    7. Singly linked list #advanced

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -36,6 +36,7 @@ class Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
+
 class SinglyLinkedList:
     """Represent a singly-linked list."""
 
@@ -60,16 +61,16 @@ class SinglyLinkedList:
         else:
             tmp = self.__head
             while (tmp.next_node is not None and
-                   tmp.next_node.data < value):
+		     tmp.next_node.data < value):
                 tmp = tmp.next_node
                 new.next_node = tmp.next_node
                 tmp.next_node = new
 
-    def __str__(self):
-        """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+def __str__(self):
+    """Define the print() representation of a SinglyLinkedList."""
+    values = []
+    tmp = self.__head
+    while tmp is not None:
+        values.append(str(tmp.data))
+        tmp = tmp.next_node
+    return ('\n'.join(values))
